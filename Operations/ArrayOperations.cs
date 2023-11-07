fbf46eedfaeb4457910372208f4bef5ab62ca817
r4f3t <parlakrafet01@gmail.com>
2023-11-07 21:15:31 +0300

    initial commit

---
+++ Operations/ArrayOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SpanUsageExample.Operations;
+
+//BenchmarkRunner.Run<ArrayOperations>();
+
+namespace SpanUsageExample.Operations
+{
+    [MemoryDiagnoser]
+    public class ArrayOperations
+    {
+        public static readonly int[] array = new int[1000000];
+        public List<int> list = new List<int>();
+
+        [Benchmark]
+        public void FillArray()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 10;
+            }
+        }
+
+        [Benchmark]
+        public void FillArrayWitSpan()
+        {
+            Span<int> span = new Span<int>(array);
+            span.Fill(10);
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SpanUsageExample.Operations;
+
+namespace SpanUsageExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //var operation = new ArrayOperations();
+        //operation.FillArray();
+        //operation.FillArrayWitSpan();
+
+        var listOperation = new ListToArrayOperations();
+        listOperation.FillList();
+        listOperation.IterateList();
+        listOperation.IterateListToArray();
+        listOperation.IterateListToArrayToSpan();
+
+        Console.ReadLine();
+    }
+}
+
